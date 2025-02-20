03503cfa83fc74ce6f6ef72b0e88d2ea4b2ed005
ErykZenith <167299127+ErykZenith@users.noreply.github.com>
2025-02-20 15:03:51 +0700

    update

---
+++ src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="bg-amber-50 z-50 cursor-pointer" @click="clk()">Click me: {{ count }}</button>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    };
+  },
+  methods:{
+    clk(){
+      console.log(1);
+      this.count++
+    }
+  }
+}
+</script>
